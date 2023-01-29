4f37a9fa37d52069e8845743b77079d397aa918c
VarshaRajput12 <varsharajput353@gmail.com>
2023-01-29 23:24:27 +0530

    yahoo clone

--- scripts/home.js
+++ scripts/home.js
@@ -82,8 +82,8 @@ const userEmail = new URL(window.location.href).searchParams.get("email");
 
 async function emailSend(e) {
   e.preventDefault();
-  const senderEmail = document.querySelector("#send_to").value;
-  const email = userEmail;
+  const  email = document.querySelector("#send_to").value;
+  const senderEmail = userEmail;
   const subject = document.querySelector("#Subject").value;
   const html = document.querySelector("#textarea").value;
 
@@ -95,7 +95,7 @@ async function emailSend(e) {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ senderEmail, email, subject, html }),
+        body: JSON.stringify({email, senderEmail, subject, html }),
       }
     );
     const data = await response.json();
@@ -108,48 +108,52 @@ async function emailSend(e) {
 
 document.querySelector("#form2").addEventListener("submit", emailSend);
 
-async function allData(e) {
-  e.preventDefault();
+// async function allData(e) {
+//   e.preventDefault();
 
-  const email = userEmail;
-  try {
-    const response = await fetch(
-      "https://aircampushack.onrender.com/gmail/listall",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-  } catch (err) {
-    console.log("Error:", err);
-  }
-}
-document.querySelector("#sent").addEventListener("click", allData);
+//   const email = userEmail;
+//   try {
+//     const response = await fetch(
+//       "https://aircampushack.onrender.com/gmail/listall",
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ email }),
+//       }
+//       );
+//       const data = await response.json();
+//       // const output = JSON.parse(data.emailData)
+//    const output = data.emailData[0].emailData;
+//    const output2 = JSON.parse(output)
+//     console.log(output2[0])
+//     console.log(output2[0].subject)
+//     console.log(output2[0].email)
 
+//   } catch (err) {
+//     console.log("Error:", err);
+//   }
+// }
+// document.querySelector("#sent").addEventListener("click", allData);
 
 const sentMails = document.querySelector(".sentMails");
 const inbox = document.querySelector(".inbox");
-document.querySelector("#sent").addEventListener("click", ()=>{
-  mails_area.style.display = "none";
-  compose_box.style.display = "none";
-  sentMails.style.display = "block"
-});
+// document.querySelector("#sent").addEventListener("click", () => {
+  // mails_area.style.display = "none";
+  // compose_box.style.display = "none";
+  // sentMails.style.display = "block";
+// });
 
-inbox.addEventListener("click",()=>{
+inbox.addEventListener("click", () => {
   compose_box.style.display = "none";
   mails_area.style.display = "block";
   sentMails.style.display = "none";
-})
+});
 
 // const Deletebtn = document.querySelectorAll(".trash");
 // const date = document.querySelectorAll(".date");
 // const mail_section = document.querySelectorAll(".mail_section");
-
 
 // mail_section.forEach((section)=>{
 //   section.addEventListener("mouseover", ()=>{
@@ -167,5 +171,91 @@ inbox.addEventListener("click",()=>{
 //   });
 // });
 
+const allData = async (e) => {
+  e.preventDefault();
 
+  mails_area.style.display = "none";
+  compose_box.style.display = "none";
+  sentMails.style.display = "block";
+  // document.querySelector(".content").style.display = "none";
+  // document.querySelector("#listall").classList.add("active");
+  // inboxBtn.classList.remove("active");
+  let email = userEmail;
 
+  try {
+    const response = await fetch(
+      "https://aircampushack.onrender.com/gmail/listall",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      }
+    );
+    const data = await response.json();
+
+    let emailData = data.emailData[0].emailData;
+    let emails = [];
+
+    try {
+      emailData = JSON.parse(emailData);
+    } catch (e) {
+      console.error("Invalid JSON string:", emailData);
+      return;
+    }
+
+    emailData.forEach((data) => {
+      if (typeof data === "string") {
+        try {
+          data = JSON.parse(data);
+        } catch (e) {
+          console.error("Invalid JSON string:", data);
+          return;
+        }
+      }
+      emails.push(data);
+    });
+
+    const emailTemplate = emails
+      .map((email) => {
+        const currentTime = new Date();
+        const hours = currentTime.getHours();
+        let ampm = "AM";
+        if (hours >= 12) {
+          ampm = "PM";
+        }
+        const currentHour = hours % 12 || 12;
+        const currentMinute = currentTime.getMinutes();
+        return `
+        <div class="mail_section">
+                <div class="left">
+                  <input type="checkbox" />
+                  <span>${email.email}</span>
+                </div>
+                <div class="mid">
+                  <i class="fa-regular fa-star"></i>
+                  <span>${email.subject}</span>
+                </div>
+                <div class="time">
+                  <i class="fa-solid fa-trash-can display"></i>
+                  <span class="date">${currentHour} : ${currentMinute}</span>
+                </div>
+              </div>
+
+    
+  `;
+      })
+      .join("");
+    document.querySelector("#sentArea_").innerHTML = emailTemplate;
+    //document.querySelector(".content2").innerHTML = emailTemplate;
+    //document.querySelector('#email-list').innerHTML = ;
+    console.log(emailTemplate);
+    console.log(data);
+    console.log("got list");
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+document.querySelector("#sent").addEventListener("click", allData);
